fe3f3b9a11e9da703bf9b277044855515baead9c
Sasikavin123 <126967032+Sasikavin123@users.noreply.github.com>
2023-03-04 20:37:24 +0530

    Add files via upload
    Week1 and Day 1 Saturday Assignment is completed

---
+++ day1/HelloSelenium.java
@@ -0,0 +1,20 @@
+package week1.day1;
+
+public class HelloSelenium {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//           System.out.println("Hello All This is my First Java Program");
+		
+		int Noofflower = 5;
+	    char type ='r';
+		float flowerperyear = 200.45f;
+         System.out.println(Noofflower);
+
+		
+		
+		
+		
+	}
+
+}
