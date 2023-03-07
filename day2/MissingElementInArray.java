58cd920c2b58a76cd16d19c015be91ed2607558c
Sasikavin123 <126967032+Sasikavin123@users.noreply.github.com>
2023-03-07 19:00:54 +0530

    Add files via upload
    Week1 Day 2 Assignment is completed

---
+++ day2/MissingElementInArray.java
@@ -0,0 +1,21 @@
+package week1.day2;
+
+import java.util.Arrays;
+
+public class MissingElementInArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       int [] arr= {1,2,3,4,7,6,8};
+       Arrays.sort(arr);
+       int n=0;
+       for (int i = 0; i < arr.length; i++) {
+    	   n++;
+    	   if (n!= arr[i])
+    	   {
+		System.out.println("Missing element :" + n);
+		break;
+	}
+	}
+
+	}}
