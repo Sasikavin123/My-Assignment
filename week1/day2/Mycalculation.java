8763cd646fd6e047aad0ed4d4732cbfe11cb50f0
Sasikavin123 <126967032+Sasikavin123@users.noreply.github.com>
2023-03-19 16:10:07 +0530

    Add files via upload
    Week1 Day 1 Assignment is completed

---
+++ week1/day2/Mycalculation.java
@@ -0,0 +1,13 @@
+package week1.day2;
+
+public class Mycalculation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Calculator obj = new Calculator();
+         System.out.println(obj.add(2,5,5));
+         System.out.println(obj.mul(7,4));
+         obj.Sub();
+	}
+
+}
